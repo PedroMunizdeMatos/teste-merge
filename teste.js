0315b60741057b4a5fe3409d185f1ba75fd6d014
Pedro Muniz <phmunizdematos@gmail.com>
2024-05-22 12:53:17 -0300

    teste pós merge da main na develop

--- teste.js
+++ teste.js
@@ -5,18 +5,19 @@ class conta {
     this.conta = conta
   }
 }
-class cliente {
-  constructor(nome, cpf, rg) {
-    this.nome = nome
-    this.cpf = cpf
-    this.rg = rg
-  }
-}
 
 class carro {
   constructor(rodas, marca, modelo) {
     this.rodas = rodas
     this.marca = marca
     this.modelo = modelo
+  }
+}
+
+class clienteSuper {
+  constructor(nome, cpf, rg) {
+    this.nome = nome
+    this.cpf = cpf
+    this.rg = rg
   }
 }
